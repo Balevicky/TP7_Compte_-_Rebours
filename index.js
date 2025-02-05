de82be593f72f021b7c82923982148e2edf9cbd5
Balevicky <balevictorien@yahoo.fr>
2025-02-05 16:37:03 +0000

    modified index.js

--- index.js
+++ index.js
@@ -1,10 +1,62 @@
+let hours = 0;
+let minutes;
+let seconds;
+let interval;
 // Créer la logique de compte à rebours
 
-choice.value = 5 minutes
+//================= Fonction de calcule
+function cacul(value) {
+  minutes = Math.floor(value / 60);
+  seconds = value % 60;
+  if (minutes > 60) {
+    hours = Math.floor(minutes / 60);
+    minutes = minutes % 60;
+  }
+}
+//================= FIN Fonction de calcule
 
-choice.value * 60 = totalSeconds
+// ======================Fonction Chrono
+const chrono = () => {
+  // ============== fonction d'affiche
+  const displayCount = () => {
+    const sec = seconds < 10 ? "0" + seconds : seconds;
+    const mn = minutes < 10 ? "0" + minutes : minutes;
+    const hh = hours < 10 ? "0" + hours : hours;
+    countdownDisplay.innerHTML = `<h2>${hh}:${mn}:${sec} </h2>`;
+  };
+  // ============== Fin fonction d'affiche
 
-let minutes = Math.floor(totalSeconds / 60);
-let seconds = totalSeconds % 60
+  if (seconds > 0) {
+    seconds--;
+    displayCount();
+  } else if (minutes > 0) {
+    seconds = 59;
+    minutes--;
 
-// Créer un événement à la validation du form pour lancer le compte à rebours
+    displayCount();
+  } else if (hours > 0) {
+    hours--;
+    minutes = 59;
+    seconds = 59;
+    displayCount();
+  } else {
+    countdownDisplay.innerHTML = `<h2>C'est terminé </h2>`;
+    clearInterval(interval);
+  }
+};
+// ====================== Fin Fonction Chrono
+
+// =============== evenement declencheur
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (isNaN(choice.value)) {
+    alert("Entrez un chiffre s'il vous plaît");
+  } else {
+    let totalSeconds = choice.value * 60;
+    choice.value = "";
+    cacul(totalSeconds);
+    clearInterval(interval);
+    interval = setInterval(chrono, 100);
+  }
+});
+// =============== evenement declencheur
